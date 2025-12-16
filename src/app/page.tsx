e4c4858780496f12c263950b3c5475f5da979048
lautaro3870 <lautarodisbro@gmail.com>
2025-12-16 18:44:41 +0100

    feat: implementation of material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Typography } from '@mui/material';
 
 export default function Home() {
   return (
     <div>
-      <h1>Holaaa</h1>
+      <Typography variant="h3">Tabla</Typography>
     </div>
   );
 }

597aaf2ce2d2e1cd8c5be758e96b4bbed57f9e83
lautaro3870 <lautarodisbro@gmail.com>
2025-12-21 17:32:20 +0100

    fix: path footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { StyledRoot } from './StyledRoot';
-import FooterBar from './components/FooterBar'
+import FooterBar from './components/Footerbar'
 
 export const metadata: Metadata = {
   title: 'Gastos',

dae019f0ee8ffe555bebb4622e27ac862cd353d3
lautaro3870 <lautarodisbro@gmail.com>
2026-01-03 15:28:26 +0100

    feat: implementation of pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default withPWA(nextConfig);
